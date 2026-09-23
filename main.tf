f64c550a4a78e8029209c978d62b111844972c1b
alexongmac <alexongmac@users.noreply.github.com>
2026-09-23 20:47:31 +0800

    Adding new S3 bucket

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-tfstate-ce13"
+    key    = "alex-31-terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "alex_s3_buc" {
+  bucket_prefix = "alex-31-bkt"
+
+  tags = {
+    Name        = "alex-31"
+    Environment = "Dev-31"
+  }
+}
